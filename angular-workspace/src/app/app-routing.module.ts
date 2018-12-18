901d77243bb4c62369562635d372cc808e0d677e
James Fogarty <jayfo@users.noreply.github.com>
2018-12-17 20:08:35 -0800

    Initial Home Content Using Markdown

--- angular-workspace/src/app/app-routing.module.ts
+++ angular-workspace/src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { MarkdownModule } from "ngx-markdown";
+
 import { HomeComponent } from "./home/home.component";
 import { ProjectsComponent } from "./projects/projects.component";
 import { CalendarComponent } from "./calendar/calendar.component";
