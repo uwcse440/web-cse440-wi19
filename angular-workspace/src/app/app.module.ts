0cc2f23bfa81676a9638fd24b763a20e167c04dc
James Fogarty <jayfo@users.noreply.github.com>
2018-12-19 12:11:53 -0800

    Begin to Stub Assignments

--- angular-workspace/src/app/app.module.ts
+++ angular-workspace/src/app/app.module.ts
@@ -10,6 +10,13 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { CalendarComponent } from './calendar/calendar.component';
+import { AssignmentsComponent } from './assignments/assignments.component';
+import { Assignment0Component } from './assignments/assignment0/assignment0.component';
+import { Assignment1Component } from './assignments/assignment1/assignment1.component';
+import { Assignment2Component } from './assignments/assignment2/assignment2.component';
+import { Assignment3Component } from './assignments/assignment3/assignment3.component';
+import { Assignment4Component } from './assignments/assignment4/assignment4.component';
+import { ReadingsComponent } from './assignments/readings/readings.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +24,14 @@ import { CalendarComponent } from './calendar/calendar.component';
     NavbarComponent,
     HomeComponent,
     ProjectsComponent,
-    CalendarComponent
+    CalendarComponent,
+    AssignmentsComponent,
+    Assignment0Component,
+    Assignment1Component,
+    Assignment2Component,
+    Assignment3Component,
+    Assignment4Component,
+    ReadingsComponent
   ],
   imports: [
     BrowserModule,
