c9b1c52b56f07c0587edf2558ff65ec7563a8dcb
James Fogarty <jayfo@users.noreply.github.com>
2018-12-19 12:11:53 -0800

    Begin to Stub Assignments

---
+++ angular-workspace/src/app/assignments/assignment0/assignment0.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { page as PageData } from './assignment0.pagedata.ts';
+
+@Component({
+  selector: 'app-assignment0',
+  templateUrl: './assignment0.component.html',
+  styleUrls: ['./assignment0.component.scss']
+})
+export class Assignment0Component implements OnInit {
+  page = PageData;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
