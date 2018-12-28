960e54826c8c5b3de580784874f048026cdbab8d
James Fogarty <jayfo@users.noreply.github.com>
2018-12-28 15:53:18 -0800

    Giving Up on Angular Compilation of Markdown

--- angular-workspace/src/app/assignments/assignment0/assignment0.component.ts
+++ angular-workspace/src/app/assignments/assignment0/assignment0.component.ts
@@ -1,9 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { page as PageData } from './assignment0.pagedata';
 
+// Markdown code until I get this organized into a reusable component
+
+import externalContent from './assignment0.content.md';
+import externalTemplate from './assignment0.component.html';
+
+import * as marked from 'marked';
+
+let renderer = new marked.Renderer();
+renderer.link = (href: string, title: string, text: string) => {
+  const isExternalLink = href.startsWith('http') || href.startsWith('//');
+
+  if (isExternalLink) {
+    return `<a href="${href}">${text}</a>`;
+  } else {
+    return `<a routerLink="${href}">${text}</a>`;
+  }
+};
+// let processedContent = marked.parse(externalContent, { renderer: renderer });
+let processedContent = 'cat';
+
+let processedTemplate: string = `
+  <ng-template #content>
+    ${processedContent}
+  </ng-template>
+  ${externalTemplate}
+`;
+
+// End Markdown code
+
 @Component({
   selector: 'app-assignment0',
-  templateUrl: './assignment0.component.html',
+  template: processedTemplate,
   styleUrls: ['./assignment0.component.scss']
 })
 export class Assignment0Component implements OnInit {
--- angular-workspace/src/app/home/home.component.ts
+++ angular-workspace/src/app/home/home.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
+// Markdown code until I get this organized into a reusable component
+
+import externalContent from './home.content.md';
+import externalTemplate from './home.component.html';
+
+    const testwork = externalContent;
+    console.log(testwork);
+    const testwork2 = externalTemplate;
+    console.log(testwork2);
+
+    /*
+let processedTemplate: string = `
+  <ng-template #content>
+    ${externalContent}
+  </ng-template>
+  ${externalTemplate}
+`;
+*/
+
+// End Markdown code
+
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html',
+  template: testwork2,
+  // templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
-
 }
