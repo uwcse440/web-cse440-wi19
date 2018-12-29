d912869550e4fa849fa7c0644d3c6d7cbdb79b34
James Fogarty <jayfo@users.noreply.github.com>
2018-12-28 16:26:44 -0800

    Preparing to Prerender

--- angular-workspace/src/app/assignments/assignments.component.ts
+++ angular-workspace/src/app/assignments/assignments.component.ts
@@ -1,44 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
-// Markdown code until I get this organized into a reusable component
-
-import externalContent from './assignments.content.md';
-import externalTemplate from './assignments.component.html';
-
-import * as marked from 'marked';
-
-let renderer = new marked.Renderer();
-renderer.link = (href: string, title: string, text: string) => {
-  const isExternalLink = href.startsWith('http') || href.startsWith('//');
-
-  if (isExternalLink) {
-    return `<a href="${href}">${text}</a>`;
-  } else {
-    return `<a routerLink="${href}">${text}</a>`;
-  }
-};
-// let processedContent = marked.parse(externalContent, { renderer: renderer });
-let processedContent = 'cat';
-
-let processedTemplate: string = `
-  <ng-template #content>
-    ${processedContent}
-  </ng-template>
-  ${externalTemplate}
-`;
-
-// End Markdown code
-
 @Component({
   selector: 'app-assignments',
-  template: processedTemplate,
+  templateUrl: './assignments.component.rendered.html',
   styleUrls: ['./assignments.component.scss']
 })
 export class AssignmentsComponent implements OnInit {
-
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
-
 }
