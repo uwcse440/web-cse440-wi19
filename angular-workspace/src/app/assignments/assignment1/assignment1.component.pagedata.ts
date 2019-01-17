ab715f2571e3d8a78acb248acd57ee428d44d708
James Fogarty <jayfo.github@gmail.com>
2019-01-17 09:25:07 -0800

    Links

--- angular-workspace/src/app/assignments/assignment1/assignment1.component.pagedata.ts
+++ angular-workspace/src/app/assignments/assignment1/assignment1.component.pagedata.ts
@@ -6,8 +6,8 @@ class PageData {
   link_project_1b: string = 'https://canvas.uw.edu/courses/1254716/assignments/4602696';
 
   due_project_1c: string = 'Submitted Wednesday, January 16, 2019';
-  link_project_1c_projects: string = ''; // https://canvas.uw.edu/courses/1173784/discussion_topics
-  link_project_1c_bids: string = ''; // https://goo.gl/forms/AdQ11KQeQpMZgDv72
+  link_project_1c_projects: string = 'https://canvas.uw.edu/courses/1254716/discussion_topics/4637096';
+  link_project_1c_bids: string = 'https://goo.gl/forms/eyapgZKMMMuotWen2';
 }
 
 export const page = new PageData();
