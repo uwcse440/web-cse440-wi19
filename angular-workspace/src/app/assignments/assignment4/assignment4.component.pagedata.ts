263ce49182fbb1804125d43aae1928915a1b4f15
annie-r <t-anross@microsoft.com>
2019-01-29 16:23:06 -0800

    add assignment 4 links
    is missing link for video submission 4d. are we still doing it as uploads since are using youtube for captions?

--- angular-workspace/src/app/assignments/assignment4/assignment4.component.pagedata.ts
+++ angular-workspace/src/app/assignments/assignment4/assignment4.component.pagedata.ts
@@ -1,20 +1,20 @@
 class PageData {
-  due_project_4a: string = ""; // Uploaded Monday, November 27, 2017
-  link_project_4a: string = ""; // https://canvas.uw.edu/courses/1173784/assignments/3960517
+  due_project_4a: string = "Uploaded Monday, March 11, 2019"; //
+  link_project_4a: string = "https://canvas.uw.edu/courses/1254716/assignments/4635400"; 
+  
+  due_project_4b: string = "Uploaded Monday, March 11, 2019";
+  link_project_4b: string = "https://canvas.uw.edu/courses/1254716/assignments/4635407"; 
 
-  due_project_4b: string = ""; // Uploaded Monday, December 4, 2017 (before class on Tuesday, December 5, 2017)
-  link_project_4b: string = ""; // https://canvas.uw.edu/courses/1173784/assignments/3960518
+  due_project_4c: string = "Uploaded Wednesday, March 13, 2019"; 
+  link_project_4c: string = "https://canvas.uw.edu/courses/1254716/assignments/4635408"; 
 
-  due_project_4c: string = ""; // Uploaded Wednesday, December 6, 2017 (before class on Thursday, December 7, 2017)
-  link_project_4c: string = ""; // https://canvas.uw.edu/courses/1173784/assignments/3960519
-
-  due_project_4d: string = ""; // Uploaded Monday, December 11, 2017
-  link_project_4d_poster: string = ""; // https://canvas.uw.edu/courses/1173784/assignments/3960521
+  due_project_4d: string = "Uploaded Friday, March 15"; // Uploaded Monday, December 11, 2017
+  link_project_4d_poster: string = "https://canvas.uw.edu/courses/1254716/assignments/4635409"; 
   link_project_4d_video: string = ""; // https://canvas.uw.edu/courses/1173784/assignments/3960522
 
-  due_project_4e: string = ""; // Submitted Monday December 12, 2017
+  due_project_4e: string = "Submitted Monday March 18. 2019"; // 
 
-  due_project_4f: string = ""; // Attend 11:00 to 12:20, Thursday, December 14, 2017
+  due_project_4f: string = "Attend 4:00-5:00, Wednesday, March 20, 2019"; 
 }
 
 export const page = new PageData();
